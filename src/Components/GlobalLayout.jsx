b004af25eca70e61fd3b21523a121e01cd569c6b
Diego Carvajal <dicarvajalb@unal.edu.co>
2024-06-06 16:26:59 -0500

    galeria al 70%, laboratorios 90% y modificaciones al header

--- src/Components/GlobalLayout.jsx
+++ src/Components/GlobalLayout.jsx
@@ -7,58 +7,72 @@ import Map2 from "./Map2";
 import Menu from "./Menu";
 import Links from "./Links";
 
-export default function GlobalLayout({PrevButton,children , title, subtitle}) {
- 
+export default function GlobalLayout({
+  PrevButton,
+  children,
+  title,
+  subtitle,
+}) {
   return (
     <div className="flex min-h-screen overflow-hidden w-[100vw] h-full flex-col items-center relative bg-gray">
-    <div className='w-[100%] h-fit bg-light menu_container '>
-      <Menu/>
-    </div>
-    <div className="flex w-full  h-fit flex-col bg-lightPurple gap-10 
-                    lg:flex-row lg:bg-white lg:items-start lg:gap-0">
-      <div className='w-full h-fit  bg-lightPurple container__map relative flex flex-col justify-start items-center
-                      lg:w-8/12 lg:items-start lg:min-h-[100vh] lg:max-h-fit lg:pe-[4rem]'>
-        <div className='container__title w-full h-[fit] ms-[1rem] lg:m-[2rem]  section_title flex flex-col justify-end items-start'>
-          <h1 className="text-white text-2xl">{subtitle || ""}</h1>
-          <h3 className="z-10 text-white text-xl lg:text-4xl font-bold">{title || "titulo"}</h3>
-          
-        </div>
-
-        <div className="container__map--parent w-[100%] h-[fit] flex flex-row justify-center lg:justify-end z-10 flex-wrap 
-                        lg:flex-nowrap">
-
-        <Map2/>
-
-        <div className='w-[0px] h-[0px] lg:ms-10 lg:w-[20rem] overflow-hidden lg:h-fit conatiner__buttons flex absolute bottom-[5rem] left-0 flex-col justify-end items-start gap-4 z-20'>
-         
-          <>
-          <BtnExternal rightIcon={"/svg/externalFilled.svg"} className={"bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple"}>Aprende sobre Metodología de proyectos y Design Thinking</BtnExternal>
-
-          <BtnExternal rightIcon={"/svg/externalFilled.svg"} className={"bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple"}>Aprende sobre los Índices de Madurez Digital</BtnExternal>
-          </>
-        </div>
-        <div className='w-fit'>
-          {children}
-        </div>
-        
-        </div>
-        <div className="w-full px-[1rem] mt-[2rem] lg:m-0">
-        {PrevButton}
-        </div>
+      <div className="w-[100%] h-fit bg-light menu_container ">
+        <Menu />
       </div>
-      <Links/>
-        
-       <div className='w-full mb-[10rem]  lg:w-[0px] bg-lightPurple overflow-hidden h-fit max-h-[5%] conatiner__buttons flex flex-col justify-end items-center border-4'>
-       
-          <div className="w-[90%]">
-          <BtnExternal rightIcon={"/svg/externalFilled.svg"} className={"bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple mb-[1rem]"}>Aprende sobre Metodología de proyectos y Design Thinking</BtnExternal>
-
-          <BtnExternal rightIcon={"/svg/externalFilled.svg"} className={"bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple"}>Aprende sobre los Índices de Madurez Digital</BtnExternal>
+      <div
+        className="flex w-full  h-fit flex-col bg-lightPurple gap-10 
+                    lg:flex-row lg:bg-white lg:items-start lg:gap-0"
+      >
+        <div
+          className="w-full h-fit  bg-lightPurple container__map relative flex flex-col justify-start items-center
+                      lg:w-8/12 lg:items-start lg:min-h-[100vh] lg:max-h-fit lg:pe-[4rem]"
+        >
+          <div className="container__title w-full h-[fit] ms-[1rem] lg:m-[2rem]  section_title flex flex-col justify-end items-start">
+            <h1 className="text-white text-2xl">{subtitle || ""}</h1>
+            <h3 className="z-10 text-white text-xl lg:text-4xl font-bold">
+              {title || "titulo"}
+            </h3>
           </div>
 
-        </div>
-    </div>
+          <div
+            className="container__map--parent w-[100%] h-[fit] flex flex-row justify-center lg:justify-end z-10 flex-wrap 
+                        lg:flex-nowrap"
+          >
+            <Map2 />
 
+            <div className="w-[0px] h-[0px] lg:ms-10 lg:w-[20rem] overflow-hidden lg:h-fit conatiner__buttons flex absolute bottom-[5rem] left-0 flex-col justify-end items-start gap-4 z-20">
+              <>
+                <BtnExternal
+                  href={
+                    "https://sites.google.com/unal.edu.co/findeter-innovacion-abierta/inicio?authuser=0"
+                  }
+                  rightIcon={"/svg/externalFilled.svg"}
+                  className={
+                    "bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple"
+                  }
+                >
+                  Convenio de innovación abierta
+                </BtnExternal>
+              </>
+            </div>
+            <div className="w-fit">{children}</div>
+          </div>
+          <div className="w-full px-[1rem] mt-[2rem] lg:m-0">{PrevButton}</div>
+        </div>
+        <Links />
+
+        <div className="w-full mb-[10rem]  lg:w-[0px] bg-lightPurple overflow-hidden h-fit max-h-[5%] conatiner__buttons flex flex-col justify-end items-center border-4">
+          <div className="w-[90%]">
+            <BtnExternal
+              rightIcon={"/svg/externalFilled.svg"}
+              className={
+                "bg-ligthAlt2 text-lightPurple font-bold hover:text-lightPurple mb-[1rem]"
+              }
+            >
+              Aprende sobre Metodología de proyectos y Design Thinking
+            </BtnExternal>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,106 +1,177 @@
-"use client"
-import { gsap } from 'gsap'
-import { useRouter } from 'next/navigation'
-import { useEffect, useRef, useState } from 'react'
-import { useIsomorphicLayoutEffect } from '../../helpers/isomorphicEffect'
-import GlobalLayout from '@/Components/GlobalLayout'
-import Map from '@/Components/Map'
-import { createPortal } from 'react-dom'
-import BtnAmazonia from '@/Components/BtnAmazonia'
-import BtnLaPaz from '@/Components/BtnLaPaz'
-import Image from 'next/image'
+"use client";
+import { gsap } from "gsap";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { useIsomorphicLayoutEffect } from "../../helpers/isomorphicEffect";
+import GlobalLayout from "@/Components/GlobalLayout";
+import Map from "@/Components/Map";
+import { createPortal } from "react-dom";
+import BtnAmazonia from "@/Components/BtnAmazonia";
+import BtnLaPaz from "@/Components/BtnLaPaz";
+import Image from "next/image";
 
-export function moveMarker(){
-  var targetPos = document.getElementsByClassName("amazonia_svg")[0]?.getClientRects()[0]
-  var selfPos = document.getElementsByClassName("btn__amazonia")[0]?.getClientRects()[0]
+export function moveMarker() {
+  var targetPos = document
+    .getElementsByClassName("amazonia_svg")[0]
+    ?.getClientRects()[0];
+  var selfPos = document
+    .getElementsByClassName("btn__amazonia")[0]
+    ?.getClientRects()[0];
 
-  console.log("[target pos]", targetPos)
+  console.log("[target pos]", targetPos);
 
-  var tMoveMarker = gsap.timeline()
-  if(targetPos && selfPos){
-  tMoveMarker.add(gsap.to(".btn__amazonia", {transformOrigin:"100% 50%", duration: 0}), 0)
-  tMoveMarker.add(gsap.to(".btn__amazonia", {top: targetPos.top + window.scrollY - selfPos.height + targetPos.height/3, duration: 0}), 0)
-  tMoveMarker.add(gsap.to(".btn__amazonia", {left: targetPos.left - selfPos.width/2 + targetPos.width/2, duration: 0}),0)
+  var tMoveMarker = gsap.timeline();
+  if (targetPos && selfPos) {
+    tMoveMarker.add(
+      gsap.to(".btn__amazonia", { transformOrigin: "100% 50%", duration: 0 }),
+      0
+    );
+    tMoveMarker.add(
+      gsap.to(".btn__amazonia", {
+        top:
+          targetPos.top +
+          window.scrollY -
+          selfPos.height +
+          targetPos.height / 3,
+        duration: 0,
+      }),
+      0
+    );
+    tMoveMarker.add(
+      gsap.to(".btn__amazonia", {
+        left: targetPos.left - selfPos.width / 2 + targetPos.width / 2,
+        duration: 0,
+      }),
+      0
+    );
   }
-  var targetPos = document.getElementsByClassName("cesar_svg")[0].getClientRects()[0]
-  if(targetPos && selfPos){
-  tMoveMarker.add(gsap.to(".btn__lapaz", {transformOrigin:"100% 50%", duration: 0}), 0)
-  tMoveMarker.add(gsap.to(".btn__lapaz", {top: targetPos.top + window.scrollY - selfPos.height + targetPos.height/3, duration: 0}), 0)
-  tMoveMarker.add(gsap.to(".btn__lapaz", {left: targetPos.left - selfPos.width/2 + targetPos.width/2, duration: 0}),0)
+  var targetPos = document
+    .getElementsByClassName("cesar_svg")[0]
+    .getClientRects()[0];
+  if (targetPos && selfPos) {
+    tMoveMarker.add(
+      gsap.to(".btn__lapaz", { transformOrigin: "100% 50%", duration: 0 }),
+      0
+    );
+    tMoveMarker.add(
+      gsap.to(".btn__lapaz", {
+        top:
+          targetPos.top +
+          window.scrollY -
+          selfPos.height +
+          targetPos.height / 3,
+        duration: 0,
+      }),
+      0
+    );
+    tMoveMarker.add(
+      gsap.to(".btn__lapaz", {
+        left: targetPos.left - selfPos.width / 2 + targetPos.width / 2,
+        duration: 0,
+      }),
+      0
+    );
   }
-  return tMoveMarker
+  return tMoveMarker;
 }
-export function moveDecoration(){
-  var targetPos = document.getElementsByClassName("svg__map")[0]?.getClientRects()[0]
-  var selfPos = document.getElementsByClassName("decoration__map")[0]?.getClientRects()[0]
+export function moveDecoration() {
+  var targetPos = document
+    .getElementsByClassName("svg__map")[0]
+    ?.getClientRects()[0];
+  var selfPos = document
+    .getElementsByClassName("decoration__map")[0]
+    ?.getClientRects()[0];
 
-  console.log("[target pos]", targetPos)
+  console.log("[target pos]", targetPos);
 
-  var tMoveMarker = gsap.timeline()
-  if(targetPos && selfPos){
-  tMoveMarker.add(gsap.to(".decoration__map", {top: targetPos.top + window.scrollY - selfPos.height/2 + targetPos.height/2, duration: 0}), 0)
-  tMoveMarker.add(gsap.to(".decoration__map", {left: targetPos.left - selfPos.width/2 + targetPos.width/2, duration: 0}),0)
+  var tMoveMarker = gsap.timeline();
+  if (targetPos && selfPos) {
+    tMoveMarker.add(
+      gsap.to(".decoration__map", {
+        top:
+          targetPos.top +
+          window.scrollY -
+          selfPos.height / 2 +
+          targetPos.height / 2,
+        duration: 0,
+      }),
+      0
+    );
+    tMoveMarker.add(
+      gsap.to(".decoration__map", {
+        left: targetPos.left - selfPos.width / 2 + targetPos.width / 2,
+        duration: 0,
+      }),
+      0
+    );
   }
 
-  return tMoveMarker
+  return tMoveMarker;
 }
-export function initialState(){
-  moveMarker()
-  moveDecoration()
-  
+export function initialState() {
+  moveMarker();
+  moveDecoration();
 }
 
-export function initialAnim(){
-  gsap.set([".btn__amazonia", ".btn__lapaz",], {scale:0, duration: 0})
-  gsap.to(".btn__lapaz", {scale:1, duration: 1})
-  gsap.to(".btn__amazonia", {scale:1, duration: 1})
-  gsap.set(".amazonia_svg", {x: 0})
+export function initialAnim() {
+  gsap.set([".btn__amazonia", ".btn__lapaz"], { scale: 0, duration: 0 });
+  gsap.to(".btn__lapaz", { scale: 1, duration: 1 });
+  gsap.to(".btn__amazonia", { scale: 1, duration: 1 });
+  gsap.set(".amazonia_svg", { x: 0 });
 }
 
 export default function Home() {
-  const amazonasRef = useRef()
-  const noInteractiveRef = useRef()
+  const amazonasRef = useRef();
+  const noInteractiveRef = useRef();
 
-  const ref = useRef()
-  
-  
-  var [body, setBody] = useState()
-  function updateBody(){
-    setBody(document.getElementById('body_id'))
-    console.log(document.getElementById('body_id'))
-    
-  } 
-  useEffect(()=>{
-    if(ref.current){
-      setBody(ref.current.contentDocument.getElementById('body_id'))
-      console.log(ref.current.contentDocument.getElementById("body_id"))
+  const ref = useRef();
+
+  var [body, setBody] = useState();
+  function updateBody() {
+    setBody(document.getElementById("body_id"));
+    console.log(document.getElementById("body_id"));
+  }
+  useEffect(() => {
+    if (ref.current) {
+      setBody(ref.current.contentDocument.getElementById("body_id"));
+      console.log(ref.current.contentDocument.getElementById("body_id"));
     }
-  },[ref])
-  
-  useIsomorphicLayoutEffect(() => {
-    window.addEventListener("resize", moveMarker)
-    window.addEventListener("resize", moveDecoration)
-      initialState()
-      initialAnim()
+  }, [ref]);
 
-  }, [])
+  useIsomorphicLayoutEffect(() => {
+    window.addEventListener("resize", moveMarker);
+    window.addEventListener("resize", moveDecoration);
+    initialState();
+    initialAnim();
+  }, []);
 
   return (
     <>
-    {console.log("ehm", process.env.rawJsFromFile)}
-      <GlobalLayout title={"Viaje hacia la construcción de entornos pacíficos y autónomos"}>
-      <div className='absolute overflow-hidden w-0 lg:w-fit top-[10rem] left-[1rem] text-white text-'>
-        <Image alt='red de laboratorios ícono' className='mx-auto  ' src={"/icons/rltp.svg"} width={"70"} height={"70"}></Image>
-        <h3 className='mx-auto w-fit'>RED DE LABORATORIOS</h3>
-        <h3 className='mx-auto w-fit'>TECNOLOGÍAS PARA LA PAZ</h3>
-      </div>
+      <GlobalLayout
+        title={"Viaje hacia la construcción de entornos pacíficos y autónomos"}
+      >
+        <div className="absolute overflow-hidden w-0 lg:w-fit top-[10rem] left-[1rem] text-white text-">
+          <Image
+            alt="red de laboratorios ícono"
+            className="mx-auto  "
+            src={"/icons/rltp.svg"}
+            width={"70"}
+            height={"70"}
+          ></Image>
+          <h3 className="mx-auto w-fit">RED DE LABORATORIOS</h3>
+          <h3 className="mx-auto w-fit">TECNOLOGÍAS PARA LA PAZ</h3>
+        </div>
       </GlobalLayout>
-      <Image alt='decoración de mapa' className="decoration__map absolute w-[30rem] lg:w-[50rem] xl:w-[60rem] z-0" src={"/svg/decorationMap.svg"} width={500} height={500}/>
+      <Image
+        alt="decoración de mapa"
+        className="decoration__map absolute w-[30rem] lg:w-[50rem] xl:w-[60rem] z-0"
+        src={"/svg/decorationMap.svg"}
+        width={500}
+        height={500}
+      />
 
-      <BtnAmazonia/>
-      <BtnLaPaz/> 
-
-     
+      <BtnAmazonia />
+      <BtnLaPaz />
     </>
-  )
+  );
 }
